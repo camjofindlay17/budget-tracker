a79505d2dffd35aaa9ed2b0735cf36d269285417
camjofindlay <camjofindlay@gmail.com>
2021-07-13 00:12:35 -0400

    updates

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
@@ -13,11 +14,13 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
 app.use(require("./routes/api.js"));
